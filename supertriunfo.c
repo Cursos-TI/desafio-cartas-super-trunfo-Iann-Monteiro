1734b2d771c51e0487bdb27673f4ccb73f7a4bfb
Iann Lucas Belle Monteiro <161357635+Iann-Monteiro@users.noreply.github.com>
2024-08-21 00:24:03 +0000

    Commit Final arrumando o que faltava

--- supertriunfo.c
+++ supertriunfo.c
@@ -1,141 +1,129 @@
 #include <stdio.h>
 
- typedef struct {
-        char nome[50];
-        double populacao;
-        double area;
-        double numPontosTuristicos;
-        double densidadePopulacional;
-        double pib;
-        double pibPerC;
+typedef struct {
+    char estado;
+    int cidade;
+    double populacao;
+    double area;
+    double pib;
+    int numPontosTuristicos;
+    double densidadePopulacional;
+    double pibPerCapita;
+    double superPoder;
 } Carta;
 
-void calculaPropriedades(Carta *c){
+void calculaPropriedades(Carta *c) {
     c->densidadePopulacional = c->populacao / c->area;
-    c->pibPerC = c->pib / c->populacao;
+    c->pibPerCapita = c->pib / c->populacao;
+    c->superPoder = c->populacao + c->area + c->pib + c->numPontosTuristicos;
 }
 
 int main() {
-   
-    Carta cartas[3];
-
-    int contadorA = 0;
-    int contadorB = 0;
+    Carta cartas[32];
+    int numCartas;
 
     printf("Bem vindo ao SUPER TRUNFO\n");
+    printf("Quantas cartas deseja cadastrar? ");
+    scanf("%d", &numCartas);
 
-    for(int i = 1; i <= 2; i++){
-
-        printf("\nInsira os Dados da Carta %d\n\n", i);
+    for (int i = 0; i < numCartas; i++) {
+        printf("\nInsira os Dados da Carta %d\n", i+1);
         
-        printf("Nome: ");
-        scanf(" %[^\n]s", cartas[i].nome);
-        getchar();
+        printf("Estado (A-H): ");
+        scanf(" %c", &cartas[i].estado);
 
-        printf("\nPopulação: ");
+        printf("Cidade (1-4): ");
+        scanf("%d", &cartas[i].cidade);
+
+        printf("População: ");
         scanf("%lf", &cartas[i].populacao);
 
-        printf("\nÁrea: ");
+        printf("Área: ");
         scanf("%lf", &cartas[i].area);
 
-        printf("\nNúmero de Pontos Turísticos: ");
-        scanf("%lf", &cartas[i].numPontosTuristicos);
-        getchar();
+        printf("Número de Pontos Turísticos: ");
+        scanf("%d", &cartas[i].numPontosTuristicos);
 
-        printf("\nPIB: ");
+        printf("PIB: ");
         scanf("%lf", &cartas[i].pib);
 
         calculaPropriedades(&cartas[i]);
     }
 
     printf("\nDados das Cartas:\n");
-    for(int i = 1; i <= 2; i++) {
-        printf("\nCarta %d:\n", i);
-        printf("Nome: %s\n", cartas[i].nome);
+    for (int i = 0; i < numCartas; i++) {
+        printf("\nCarta %d:\n", i+1);
+        printf("Estado: %c, Cidade: %d\n", cartas[i].estado, cartas[i].cidade);
         printf("População: %.2f\n", cartas[i].populacao);
         printf("Área: %.2f\n", cartas[i].area);
-        printf("Número de Pontos Turísticos: %f\n", cartas[i].numPontosTuristicos);
+        printf("Número de Pontos Turísticos: %d\n", cartas[i].numPontosTuristicos);
         printf("Densidade Populacional: %.2f\n", cartas[i].densidadePopulacional);
         printf("PIB: %.2f\n", cartas[i].pib);
-        printf("PIB per Capita: %.2f\n", cartas[i].pibPerC);
+        printf("PIB per Capita: %.2f\n", cartas[i].pibPerCapita);
+        printf("Super Poder: %.2f\n", cartas[i].superPoder);
     }
 
-    printf("\nQual carta é a mais forte?\n\n");
-    printf("Densidade Populacional: \n");
-    if(cartas[1].densidadePopulacional > cartas[2].densidadePopulacional){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else if(cartas[1].densidadePopulacional < cartas[2].densidadePopulacional){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else{
+    int carta1, carta2;
+    printf("\nEscolha duas cartas para comparar (0 a %d): ", numCartas-1);
+    scanf("%d %d", &carta1, &carta2);
+
+    printf("\nComparando cartas %d e %d:\n", carta1+1, carta2+1);
+
+    printf("\nDensidade Populacional: \n");
+    if (cartas[carta1].densidadePopulacional < cartas[carta2].densidadePopulacional)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].densidadePopulacional > cartas[carta2].densidadePopulacional)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
         printf("Empate!\n");
-    }
 
-    printf("\nOutras Propriedades: \n");
     printf("\nPopulação: \n");
-    if(cartas[1].populacao > cartas[2].populacao){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else if(cartas[1].populacao < cartas[2].populacao){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else{
+    if (cartas[carta1].populacao > cartas[carta2].populacao)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].populacao < cartas[carta2].populacao)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
         printf("Empate!\n");
-    }
 
     printf("\nÁrea: \n");
-    if(cartas[1].area > cartas[2].area){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else if(cartas[1].area < cartas[2].area){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else{
+    if (cartas[carta1].area > cartas[carta2].area)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].area < cartas[carta2].area)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
         printf("Empate!\n");
-    }
 
     printf("\nNúmero de Pontos Turísticos: \n");
-    if(cartas[1].numPontosTuristicos > cartas[2].numPontosTuristicos){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else if(cartas[1].numPontosTuristicos < cartas[2].numPontosTuristicos){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else{
+    if (cartas[carta1].numPontosTuristicos > cartas[carta2].numPontosTuristicos)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].numPontosTuristicos < cartas[carta2].numPontosTuristicos)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
         printf("Empate!\n");
-    }
-    
-    printf("\nPib: \n");
-    if(cartas[1].pib > cartas[2].pib){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else if(cartas[1].pib < cartas[2].pib){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else{
-        printf("Empate!\n");
-    }
 
-    printf("\nPib per Capita: \n");
-    if(cartas[1].pibPerC > cartas[2].pibPerC){
-        printf("Carta %s Venceu!\n", cartas[1].nome);
-        contadorA++;
-    }else if(cartas[1].pibPerC < cartas[2].pibPerC){
-        printf("Carta %s Venceu!\n", cartas[2].nome);
-        contadorB++;
-    }else{
+    printf("\nPIB: \n");
+    if (cartas[carta1].pib > cartas[carta2].pib)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].pib < cartas[carta2].pib)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
         printf("Empate!\n");
-    }
 
-    printf("\nPlacar Total: \n");
-    if(contadorA > contadorB){
-        printf("Carta %s Venceu! Resultado: %d a %d\n", cartas[1].nome, contadorA, contadorB);
-    }else if(contadorA < contadorB){
-        printf("Carta %s Venceu!Resultado: %d a %d\n", cartas[2].nome, contadorA, contadorB);
-    }else{
-        printf("Empate! Resultado: %d a %d\n", contadorA, contadorB);
-    }
+    printf("\nPIB per Capita: \n");
+    if (cartas[carta1].pibPerCapita > cartas[carta2].pibPerCapita)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].pibPerCapita < cartas[carta2].pibPerCapita)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
+        printf("Empate!\n");
+
+    printf("\nSuper Poder: \n");
+    if (cartas[carta1].superPoder > cartas[carta2].superPoder)
+        printf("Carta %d venceu!\n", carta1+1);
+    else if (cartas[carta1].superPoder < cartas[carta2].superPoder)
+        printf("Carta %d venceu!\n", carta2+1);
+    else
+        printf("Empate!\n");
 
     return 0;
 }
